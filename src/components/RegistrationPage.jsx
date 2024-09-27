89b6c8fd68a79c0544e462bc0fd61587b82d5ff6
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-27 21:35:12 +0200

    fixed registration url

--- src/components/RegistrationPage.jsx
+++ src/components/RegistrationPage.jsx
@@ -6,122 +6,81 @@ function RegistrationPage() {
   const [userData, setUserData] = useState({
     username: "",
     email: "",
-    password: "", 
+    password: "",
     name: "",
-    surname: ""
-  })
+    surname: "",
+  });
 
-  const navigate = useNavigate()
-  const url = "http://localhost:8080/auth/register"
+  const navigate = useNavigate();
+  const url = import.meta.env.VITE_URL;
 
-  const register = async () =>{
-    try{
-      const resp = await fetch(url, {
+  const register = async () => {
+    try {
+      const resp = await fetch(url + "/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-        }, 
+        },
         body: JSON.stringify(userData),
-    });
+      });
       if (resp.ok) {
         navigate("/");
       }
-    }catch (error){
+    } catch (error) {
       console.log(error);
-      
     }
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    register()
-    }
-     
+    register();
+  };
 
   const handleChange = (e) => {
-    setUserData({...userData, [e.target.name]: e.target.value})
-
+    setUserData({ ...userData, [e.target.name]: e.target.value });
   };
 
   return (
-    <div className="registration-page"> 
-    <Container className="d-flex align-items-center justify-content-center">
-     
-      <Row className="justify-content-center mt-5">
-        <Col md={12}>
-         <div className="form-container">
-          <h2 className="text-center mb-4">Registrazione</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter username"
-                name="username"
-                value={userData.username}
-                onChange={handleChange}
-                required 
-              />
-            </Form.Group>
+    <div className="registration-page">
+      <Container className="d-flex align-items-center justify-content-center">
+        <Row className="justify-content-center mt-5">
+          <Col md={12}>
+            <div className="form-container">
+              <h2 className="text-center mb-4">Registrazione</h2>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicUsername">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control type="text" placeholder="Enter username" name="username" value={userData.username} onChange={handleChange} required />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                name="email"
-                value={userData.email}
-                onChange={handleChange}
-                required 
-              />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" name="email" value={userData.email} onChange={handleChange} required />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter password"
-                name="password"
-                value={userData.password}
-                onChange={handleChange}
-                required 
-              />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Enter password" name="password" value={userData.password} onChange={handleChange} required />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter name"
-                name="name"
-                value={userData.name}
-                onChange={handleChange}
-                required 
-              />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter name" name="name" value={userData.name} onChange={handleChange} required />
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicSurname">
-              <Form.Label>Surname</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter surname"
-                name="surname"
-                value={userData.surname}
-                onChange={handleChange}
-                required 
-              />
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicSurname">
+                  <Form.Label>Surname</Form.Label>
+                  <Form.Control type="text" placeholder="Enter surname" name="surname" value={userData.surname} onChange={handleChange} required />
+                </Form.Group>
 
-          
-
-            <Button variant="primary" type="submit" className="w-100">
-              Register
-            </Button>
-          </Form>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+                <Button variant="primary" type="submit" className="w-100">
+                  Register
+                </Button>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
