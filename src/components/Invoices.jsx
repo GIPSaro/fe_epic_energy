f5079ddfb54c6bd17be1f0fc0ade0e52fa47e8e7
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-27 15:31:31 +0200

    invoice filter ok

--- src/components/Invoices.jsx
+++ src/components/Invoices.jsx
@@ -5,25 +5,21 @@ import { toast } from "react-toastify";
 
 const Invoices = () => {
   const [invoices, setInvoices] = useState([]);
-  const [value, setValue] = useState("")
-  const [showClient, setShowClient] = useState(false)
-  const [showState, setShowState] = useState(false)
-  const [showData, setShowData] = useState(false)
-  const [showYear, setShowYear] = useState(false)
-  const [showRangeAmount, setShowRangeAmount] = useState(false)
+  const [value, setValue] = useState("");
+  const [showClient, setShowClient] = useState(false);
+  const [showState, setShowState] = useState(false);
+  const [showData, setShowData] = useState(false);
+  const [showYear, setShowYear] = useState(false);
+  const [showRangeAmount, setShowRangeAmount] = useState(false);
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
   const url = import.meta.env.VITE_URL;
 
-
-  const notifyError = (errorMsg) =>{
+  const notifyError = (errorMsg) => {
     toast.error(errorMsg, {
       className: "text-white bg-danger m-4",
-    })
-  }
-
-  function formatDate(dateString) {
-    const [day, month, year] = dateString.split('/');
-    return `${year}-${month}-${day}`;
-}
+    });
+  };
 
   const getallInvoices = async () => {
     const resp = await fetch(url + "/invoices", {
@@ -38,184 +34,227 @@ const Invoices = () => {
   };
 
   const filterByClient = async () => {
-  try {  const resp = await fetch(url + "/invoices?clientId=" + value, {
+    try {
+      const resp = await fetch(url + "/invoices?clientId=" + value, {
         headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-        }, 
-    });
-    if(resp.ok){
-        const res = await resp.json()
-        setInvoices(res.content)
-    }} catch (error){
-        console.log(error);
-        notifyError("Errore nel reperimento dei dati")
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (resp.ok) {
+        const res = await resp.json();
+        setInvoices(res.content);
+      }
+    } catch (error) {
+      console.log(error);
+      notifyError("Errore nel reperimento dei dati");
     }
-  }
+  };
 
   const filterByState = async () => {
-    try {  const resp = await fetch(url + "/invoices?invoicesState=" + value, {
-          headers: {
-              Authorization: "Bearer " + localStorage.getItem("token"),
-          }, 
+    try {
+      const resp = await fetch(url + "/invoices/state?state=" + value, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
       });
-      if(resp.ok){
-          const res = await resp.json()
-          setInvoices(res.content)
-      }} catch (error){
-          console.log(error);
-          notifyError("Errore nel reperimento dei dati")
+      if (resp.ok) {
+        const res = await resp.json();
+        setInvoices(res.content);
       }
+    } catch (error) {
+      console.log(error);
+      notifyError("Errore nel reperimento dei dati");
     }
+  };
 
-    const filterByDate = async () => {
-        try {  const resp = await fetch(url + "/invoices?date=" + value, {
-              headers: {
-                  Authorization: "Bearer " + localStorage.getItem("token"),
-              }, 
-          });
-          if(resp.ok){
-              const res = await resp.json()
-              setInvoices(res.content)
-          }} catch (error){
-              console.log(error);
-              notifyError("Errore nel reperimento dei dati")
-          }
-        }
-    const filterByYear = async () => {
-            try {  const resp = await fetch(url + "/invoices?year=" + value, {
-                  headers: {
-                      Authorization: "Bearer " + localStorage.getItem("token"),
-                  }, 
-              });
-              if(resp.ok){
-                  const res = await resp.json()
-                  setInvoices(res.content)
-              }} catch (error){
-                  console.log(error);
-                  notifyError("Errore nel reperimento dei dati")
-              }
-            }
-    const filterByRangeAmount = async () => {
-                try {  const resp = await fetch(url + "/invoices?amountRange=" + value, {
-                      headers: {
-                          Authorization: "Bearer " + localStorage.getItem("token"),
-                      }, 
-                  });
-                  if(resp.ok){
-                      const res = await resp.json()
-                      setInvoices(res.content)
-                  }} catch (error){
-                      console.log(error);
-                      notifyError("Errore nel reperimento dei dati")
-                  }
-                }
+  const filterByDate = async () => {
+    try {
+      const resp = await fetch(url + "/invoices/date?date=" + value, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (resp.ok) {
+        const res = await resp.json();
+        setInvoices(res.content);
+      }
+    } catch (error) {
+      console.log(error);
+      notifyError("Errore nel reperimento dei dati");
+    }
+  };
+  const filterByYear = async () => {
+    try {
+      const resp = await fetch(url + "/invoices/year?year=" + value, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (resp.ok) {
+        const res = await resp.json();
+        setInvoices(res.content);
+      }
+    } catch (error) {
+      console.log(error);
+      notifyError("Errore nel reperimento dei dati");
+    }
+  };
+  const filterByRangeAmount = async () => {
+    try {
+      const resp = await fetch(url + "/invoices/amountRange?min=" + min + "&max=" + max, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (resp.ok) {
+        const res = await resp.json();
+        setInvoices(res.content);
+      }
+    } catch (error) {
+      console.log(error);
+      notifyError("Errore nel reperimento dei dati");
+    }
+  };
 
-  const handleChange = e => {
-    setValue(e.target.value)
-  }
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-  const handleChangeData = e => {
-    setValue(formatDate(e.target.value))
-  }
+  const handleMin = (e) => {
+    setMin(e.target.value);
+  };
 
-  const handlefetchByClient = e => {
-    e.preventDefault()
-    filterByState()
-  }
+  const handleMax = (e) => {
+    setMax(e.target.value);
+  };
 
-  const handlefetchByState = e => {
-    e.preventDefault()
-    filterByClient()
-  }
-  const handlefetchByDate = e => {
-    e.preventDefault()
-    filterByDate()
-  }
-  const handlefetchByYear = e => {
-    e.preventDefault()
-    filterByYear()
-  }
-  const handlefetchByRangeAmount = e => {
-    e.preventDefault()
-    filterByRangeAmount()
-  }
+  const handlefetchByClient = (e) => {
+    e.preventDefault();
+    filterByClient();
+  };
+
+  const handlefetchByState = (e) => {
+    e.preventDefault();
+    filterByState();
+  };
+  const handlefetchByDate = (e) => {
+    e.preventDefault();
+    filterByDate();
+  };
+  const handlefetchByYear = (e) => {
+    e.preventDefault();
+    filterByYear();
+  };
+  const handlefetchByRangeAmount = (e) => {
+    e.preventDefault();
+    filterByRangeAmount();
+  };
 
   useEffect(() => {
-    getallInvoices()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
+    getallInvoices();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-    
-    <SideNavbar/>
+      <SideNavbar />
       <Container className="mt-5">
         <h1>Invoices</h1>
         <DropdownButton className="mb-2" id="dropdown-basic-button" title="Filters">
-        <Dropdown.Item onClick={() => {
-            setShowClient(true)
-            setShowState(false);
-             setShowData(false); 
-             setShowYear(false); 
-             setShowRangeAmount(false)}}>Client</Dropdown.Item>
-        <Dropdown.Item onClick={() => {
-            setShowClient(false);
-             setShowState(true);
-             setShowData(false)
-             setShowYear(false); 
-             setShowRangeAmount(false) }}>State</Dropdown.Item>
-        <Dropdown.Item onClick={() => {
-            setShowState(false);
-             setShowData(true)
-             setShowClient(false);
-             setShowYear(false); 
-             setShowRangeAmount(false) }}>Date</Dropdown.Item>
-              <Dropdown.Item onClick={() => {
-            setShowState(false);
-             setShowData(false)
-             setShowClient(false); 
-             setShowYear(true); 
-             setShowRangeAmount(false)}}>Year</Dropdown.Item>
-              <Dropdown.Item onClick={() => {
-            setShowState(false);
-             setShowData(false)
-             setShowClient(false); 
-             setShowYear(false); 
-             setShowRangeAmount(true)}}>Range Amount</Dropdown.Item>
-      </DropdownButton>
-      
+          <Dropdown.Item
+            onClick={() => {
+              setShowClient(true);
+              setShowState(false);
+              setShowData(false);
+              setShowYear(false);
+              setShowRangeAmount(false);
+            }}
+          >
+            Client
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              setShowClient(false);
+              setShowState(true);
+              setShowData(false);
+              setShowYear(false);
+              setShowRangeAmount(false);
+            }}
+          >
+            State
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              setShowState(false);
+              setShowData(true);
+              setShowClient(false);
+              setShowYear(false);
+              setShowRangeAmount(false);
+            }}
+          >
+            Date
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              setShowState(false);
+              setShowData(false);
+              setShowClient(false);
+              setShowYear(true);
+              setShowRangeAmount(false);
+            }}
+          >
+            Year
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              setShowState(false);
+              setShowData(false);
+              setShowClient(false);
+              setShowYear(false);
+              setShowRangeAmount(true);
+            }}
+          >
+            Range Amount
+          </Dropdown.Item>
+        </DropdownButton>
 
-      <Form className={showClient ? "d-block" : "d-none"} onSubmit={handlefetchByClient}>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Client</Form.Label>
-          <Form.Control type="text" placeholder="Client" value={value} onChange={handleChange} />
-        </Form.Group>
+        <Form className={showClient ? "d-block" : "d-none"} onSubmit={handlefetchByClient}>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Client</Form.Label>
+            <Form.Control type="text" placeholder="Client" value={value} onChange={handleChange} />
+          </Form.Group>
         </Form>
         <Form className={showState ? "d-block" : "d-none"} onSubmit={handlefetchByState}>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" value={value} onChange={handleChange} />
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>State</Form.Label>
+            <Form.Control type="text" placeholder="State" value={value} onChange={handleChange} />
+          </Form.Group>
         </Form>
         <Form className={showData ? "d-block" : "d-none"} onSubmit={handlefetchByDate}>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Date</Form.Label>
-          <Form.Control type="date" placeholder="Date" value={value} onChange={handleChangeData} />
-        </Form.Group>
-      </Form>
-      <Form className={showYear ? "d-block" : "d-none"} onSubmit={handlefetchByYear}>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Year</Form.Label>
-          <Form.Control type="text" placeholder="Year" value={value} onChange={handleChange} />
-        </Form.Group>
-      </Form>
-      <Form className={showRangeAmount ? "d-block" : "d-none"} onSubmit={handlefetchByRangeAmount}>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Range Amount</Form.Label>
-          <Form.Control type="number" placeholder="Range Amount" value={value} onChange={handleChange} />
-        </Form.Group>
-      </Form>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Date</Form.Label>
+            <Form.Control type="text" placeholder="Date" value={value} onChange={handleChange} />
+          </Form.Group>
+        </Form>
+        <Form className={showYear ? "d-block" : "d-none"} onSubmit={handlefetchByYear}>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Year</Form.Label>
+            <Form.Control type="text" placeholder="Year" value={value} onChange={handleChange} />
+          </Form.Group>
+        </Form>
+        <Form className={showRangeAmount ? "d-block" : "d-none"} onSubmit={handlefetchByRangeAmount}>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Min Amount</Form.Label>
+            <Form.Control type="text" placeholder="min" value={min} onChange={handleMin} />
+          </Form.Group>
+        </Form>
+        <Form className={showRangeAmount ? "d-block" : "d-none"} onSubmit={handlefetchByRangeAmount}>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Max Amount</Form.Label>
+            <Form.Control type="text" placeholder="max" value={max} onChange={handleMax} />
+          </Form.Group>
+        </Form>
+
         <Table striped bordered hover>
           <thead>
             <tr>
